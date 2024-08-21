9eb5cb5f7ab4423bb0602fa5785f73af3ac728ef
David Chardon <121019053+ASyhnes@users.noreply.github.com>
2024-08-21 23:14:12 +0200

    Update AlgoCoordonateUltraD.py
    mise en place des deux principales problématiques auquelles le projet doit répondre

--- AlgoUltraD/AlgoCoordonateUltraD.py
+++ AlgoUltraD/AlgoCoordonateUltraD.py
@@ -1,12 +1,11 @@
-#1)créer une fonction qui génére un nombre de dimention
-# de taille dépendante du chiffre à classer (possibilitées infinies>créer par l'utilisateur en #4)
-#2)créer une fonction qui génére les coordonnées de dimention ** X en fonction du chiffre entré en #4
-#3)creer une fonction qui va creer un model 3D àpartir des fonction précédentes.
-#4)demander à l'utilisateur de rentrer un chiffre
-#5)creer un référentiel 3D de dimention ** X
-#6)placer un point dans le référentiel en focntion des coordonné créer par la fonction qui génére les coordonnée.
-#7)mémoriser le chiffre
-#8)rentrer un autre chiffre/suite de chiffre/liste pour voir les modéles générer
-# (par exemple que se passe t il si on rentre une suite de nombre premier???)
+# premiére problématique: (la moins importante) trouver une maniére de comprésser les donnée du nombre en une coordonnée à trois chiffre + une clef.
 
+# nombre de dimention 0: compris entre 0 et 999. coordonnée X,Y,Z sur dimention interne
+# noombre de dimention 1: compris entre 0 et 999,999,999. coordonnée 1,X,Y,Z sur dimention supérieure=> 
+  # comment imbriquer les chiffre dans les zero
+    # => par exemple 876,899,123: comment classer 876 dans le z en une seule unité, 899 dans y et 123 dans X
+      #  =>piste de réflexion : le résultat d'un modulo ou une division entre 
 
+# noombre de dimention 2: compris entre 0 et 999,999,999,999. coordonnée ?????
+
+# deuxiéme problématique: générer une visualisation dans l'espace d'un empilement de cube dans un autre empillement de cube sous forme de dimention qui se replie sur elle même.
