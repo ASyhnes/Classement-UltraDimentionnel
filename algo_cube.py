fb9971aa548453d6e776295ac3479904af1db638
ASyhnes <syhnes@gmail.com>
2024-08-23 11:03:09 +0200

    add readme

--- algo_cube.py
+++ algo_cube.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-<<<<<<< HEAD
 import numpy as np
 
 # Définir la classe Cube
@@ -76,31 +75,6 @@ try:
 except ValueError as e:
     print(f"Erreur: {e}. Réessayez.")
     exit()
-=======
-
-print ("##############################################")
-print ("##############################################")
-print ("#####                                   ######")
-print ("#####           génération              ######")
-print ("#####       Cube de dimention           ######")
-print ("#####                1                  ######")
-print ("#####         entre 0 et 999            ######")
-print ("#####                                   ######")
-print ("##############################################")
-print ("##############################################")
-print ("")
-print ("")
-
-# Génération de la coordoné du nombre
-def number_to_coordinates(n):
-    if 0 <= n <= 999:
-        z = n // 100
-        y = (n % 100) // 10
-        x = n % 10
-        return x, y, z
-    else:
-        raise ValueError("Please, input betwwen 0 and 999")
->>>>>>> 69f0ab811253fa7ad2b20420ef51d148c54bec2a
 
 # Configuration de la figure 3D
 fig = plt.figure()
@@ -111,7 +85,6 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
-<<<<<<< HEAD
 # Fixer le ratio des axes pour que le cube apparaisse proportionnel
 ax.set_box_aspect([1,1,1])  
 # Maintient une échelle égale sur les axes
@@ -130,36 +103,4 @@ for (x, y, z) in cube1.get_points():
 draw_cube(ax, cube2_x, cube2_y, cube2_z, 10)
 
 # Affichage final
-=======
-# Affichage du cube
-ax.set_xlim(0, 9)
-ax.set_ylim(0, 9)
-ax.set_zlim(0, 9)
-
-# Espace pour entrer des nombres
-while True:
-    user_input = input("Entrez un nombre entre 0 et 999 (ou 'q' pour quitter) : ")
-
-    if user_input.lower() == 'q':
-        break
-
-    try:
-        number = int(user_input)
-        x, y, z = number_to_coordinates(number)
-
-        # Affichage du point
-        ax.scatter(x, y, z, c='blue', marker='o')
-        ax.text(x, y, z, str(number), size=10, zorder=1, color='k')
-
-        # Maj d'affichage
-        plt.draw()
-        # decommenter si possible et revérifier utilité
-        # plt.pause(0.5)
-
-    except ValueError as e:
-        print(f"Erreur: {e}. Veuillez entrer un nombre valide.")
-        continue
-
-# Figure finale (à customiser)
->>>>>>> 69f0ab811253fa7ad2b20420ef51d148c54bec2a
 plt.show()
